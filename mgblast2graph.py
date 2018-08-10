6a39a822c131e53459dca94f8d4824de9a60160e
Miloš Šimek <tjsnake@me.com>
2018-08-10 13:23:12 +0200

    Adds kick-start script

--- mgblast2graph.py
+++ mgblast2graph.py
@@ -1,3 +1,17 @@
-#/usr/bin/env python3
+#!/usr/bin/env python3
+
+"""This is Python reimplementation of R function mgblast2graph"""
+
+def mgblast2graph(in_blastFile, in_seqFile):
+	pass
 
 
+if __name__ == '__main__':
+	"""Run the code on sample data"""
+
+	params = {}
+	inputFolder = "input-data/"
+	params["in_blastFile"] = inputFolder + "in-blast.csv"
+	params["in_seqFile"] = inputFolder + "in-reads-fas"
+
+	mgblast2graph(**params)
